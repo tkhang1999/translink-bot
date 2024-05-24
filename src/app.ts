18aaa4888b3d04c7a70a5fc9c220db086021e08f
Khang Le <tkhang1999@gmail.com>
2024-05-23 17:47:28 -0700

    use backup translink APIs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import { Context, Markup, Telegraf } from "telegraf";
-import { Location, Update } from "typegram";
+import { Context, Telegraf } from "telegraf";
+import { Update } from "typegram";
 import { message } from "telegraf/filters";
 import dotenv from "dotenv";
-import { getBusArrivalTime, getNearby, NearbyResponse } from "./schedules";
+import { getBusArrivalTime } from "./arrival";
 
 dotenv.config();
 
@@ -11,29 +11,9 @@ const bot: Telegraf<Context<Update>> = new Telegraf(
 );
 
 bot.start((ctx) => {
-  const message: string = `Welcome! Please use the command /nearby to find nearby bus stops and schedules.\n
-Or reply with the below format to query bus arrival time:
+  const message = `Welcome! Please reply with the below format to query bus arrival time:
 "<bus_id> <bus_stop_id>" (e.g. "145 59314")`;
   ctx.reply(message);
-});
-
-bot.command("nearby", async (ctx) => {
-  return ctx.reply(
-    "Please share your location",
-    Markup.keyboard([Markup.button.locationRequest("Send location")])
-      .resize(true)
-      .oneTime(true)
-  );
-});
-
-bot.on(message("location"), async (ctx) => {
-  const location: Location.CommonLocation = ctx.message.location;
-  const nearbyResponse: NearbyResponse = await getNearby(location);
-
-  ctx.reply(
-    nearbyResponse.message,
-    Markup.keyboard(nearbyResponse.options).resize(true).oneTime(false)
-  );
 });
 
 bot.on(message("text"), async (ctx) => {
---
+++ src/arrival.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosError } from "axios";
+import moment, { Moment } from "moment-timezone";
+import { get } from "lodash";
+
+export const getBusArrivalTime = async (input: string[]): Promise<string> => {
+  if (input.length !== 2) {
+    console.log(input);
+    return "Wrong input format! Please try again!";
+  }
+
+  const current: Moment = moment().tz("America/Vancouver");
+  const date: string = current.format("YYYY-MM-DD");
+
+  const busId: string = input[0];
+  const stopId: string = input[1];
+  const url = `https://getaway.translink.ca/api/gtfs/stop/${stopId}/route/${busId}/realtimeschedules?querySize=6`;
+
+  try {
+    const res = await axios({ method: "get", url });
+    const data: any[] = get(res, "data[0].r[0].t", []);
+    if (data.length === 0) {
+      return "No schedule times available! Again one more time?";
+    }
+
+    let message = `Bus ${busId} at stop ${stopId}:\n`;
+    data.map((entry: any) => {
+      let next: Moment = moment(`${date} ${entry.dt}`).tz("America/Vancouver");
+      let diff: number = next.diff(current, "minutes");
+      if (diff < -10) {
+        next = next.add(1, "days");
+        diff = next.diff(current, "minutes");
+      }
+
+      if (diff < 0) {
+        message += `\n${-diff} mins ago (${next.format("hh:mm A")})`;
+      } else if (diff === 0) {
+        message += `\nnow (${next.format("hh:mm A")})`;
+      } else {
+        message += `\nin ${diff} mins (${next.format("hh:mm A")})`;
+      }
+    });
+
+    return message;
+  } catch (error) {
+    console.log(error);
+    let message = "Something unexpectedly happened :(";
+    if (axios.isAxiosError(error)) {
+      const axiosError: AxiosError = error as AxiosError;
+      message = get(axiosError.response?.data, "Message", message);
+    }
+    return message;
+  }
+};
